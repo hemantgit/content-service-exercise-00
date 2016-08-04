60217c94a77022c239141ea97d0e8230eb0d58b8
Marcio Fuckner <marcio@backbase.com>
2016-08-04 22:37:18 +0200

    First commit

---
+++ src/main/java/com/backbase/training/cs/SimpleClient.java
@@ -0,0 +1,46 @@
+package com.backbase.training.cs;
+
+import org.apache.chemistry.opencmis.client.api.*;
+import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
+import org.apache.chemistry.opencmis.commons.SessionParameter;
+import org.apache.chemistry.opencmis.commons.data.PropertyData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by marcio on 03/08/16.
+ */
+public class SimpleClient {
+
+    public static void main(String[] args) {
+
+        Map<String, String> connProps = new HashMap<>();
+
+        connProps.put(SessionParameter.BINDING_TYPE,"atompub");
+        connProps.put(SessionParameter.ATOMPUB_URL, "http://localhost:7777/portalserver/content/atom");
+        connProps.put(SessionParameter.USER, "admin");
+        connProps.put(SessionParameter.PASSWORD, "admin");
+        connProps.put(SessionParameter.REPOSITORY_ID,"ad4a53ad-fe45-4519-9b9c-cd50452efee2");
+
+        SessionFactory factory = SessionFactoryImpl.newInstance();
+        Session session = factory.createSession(connProps);
+
+        QueryStatement qs = session.createQueryStatement("SELECT * FROM cmis:document");
+
+        ItemIterable<QueryResult> results = qs.query(false);
+
+        System.out.println("results: " + results.getTotalNumItems());
+
+        for(QueryResult hit: results) {
+            for(PropertyData<?> property: hit.getProperties()) {
+
+                String queryName = property.getQueryName();
+                Object value = property.getFirstValue();
+
+                System.out.println(queryName + ": " + value);
+            }
+            System.out.println("--------------------------------------");
+        }
+    }
+}
